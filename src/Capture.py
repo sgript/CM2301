7b77c0b0f5063d17845b6c7234f24d7d890255e1
shazaib <S94Ahmad@gmail.com>
2015-02-01 18:02:37 +0000

    Updated files

---
+++ src/Capture.py
@@ -0,0 +1,47 @@
+'''
+This file will open the webcam up, then extract the image, this image then will be cropped
+so that it only shows the face of the person, which can then later be compared.
+
+Phase I - Capture image
+'''
+
+__author__ = "shazaibahmad"
+
+import cv2 as cv
+
+def ImageFromCam():
+	cap = cv.VideoCapture(0)
+	cap.set(1, 20.0)
+	cap.set(3,640)  
+	cap.set(4,480) 
+
+	 
+	Face_Cascade = cv.CascadeClassifier('../cv/haarcascade_frontalface_default.xml')
+	#Eye_Cascade = cv.CascadeClassifier('./cv/haarcascade_eye.xml') # Later may add eyes.
+
+
+	while True:
+	    # Capture frame-by-frame
+	    ret, frame = cap.read()
+
+	    Gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+	    Faces = Face_Cascade.detectMultiScale(Gray, scaleFactor = 1.1, minNeighbors = 5, minSize = (30, 30), flags = cv.cv.CV_HAAR_SCALE_IMAGE)
+
+	    # Draw a rectangle around the faces
+	    for (x, y, w, h) in Faces: # To draw rectangle for face
+	        cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+	    # Display the resulting frame
+	    cv.imshow('Webcam - Active (Press Q to finish)', frame)
+	    cv.imwrite('../img/face.jpg', frame)
+
+	    if cv.waitKey(1) & 0xFF == ord('q'):
+	        break
+
+	# When everything is done, release the capture
+	cap.release()
+	cv.destroyAllWindows()
+
+ImageFromCam()
+execfile('Crop.py')
---
+++ src/Crop.py
@@ -0,0 +1,88 @@
+'''
+This file will open the webcam up, then extract the image, this image then will be cropped
+so that it only shows the face of the person, which can then later be compared.
+
+Phase II - Crop face out
+'''
+
+import cv 
+from PIL import Image 
+import glob
+import os
+
+def Detect_Face(Img, faceCascade, Give_Image = False):
+    # Detects face with haarcascade -- http://www.lucaamore.com/?p=638
+
+    # Set haarcascade object options    
+    HaarScale = 1.1
+    HaarFlags = 0
+    MinSize = (20, 20)
+    MinNeighbouring = 3
+
+    # Equalize the histogram
+    cv.EqualizeHist(Img, Img)
+
+    # Detection of the user's face
+    Faces = cv.HaarDetectObjects(
+            Img, faceCascade, cv.CreateMemStorage(0),
+            HaarScale, MinNeighbouring, HaarFlags, MinSize)
+
+    # Draw rectangles when found 
+    if Faces and Give_Image:
+        for ((x, y, w, h), n) in Faces:
+            # Set two CV Points in order to draw the rectangular box.
+            Point1 = (int(x), int(y))
+            Point2 = (int(x + w), int(y + h))
+            cv.Rectangle(Img, Point1, Point2, cv.RGB(255, 0, 0), 5, 8, 0)
+
+
+    if Give_Image:
+        return Img
+    else:
+        return Faces
+
+def Grey_Scale(PIL_Image):
+    # Turn into a greyscale image - http://pythonpath.wordpress.com/2012/05/08/pil-to-opencv-image/
+    PIL_Image = PIL_Image.convert('L')
+    OpenCV_Image = cv.CreateImageHeader(PIL_Image.size, cv.IPL_DEPTH_8U, 1)
+    cv.SetData(OpenCV_Image, PIL_Image.tostring(), PIL_Image.size[0])
+    return OpenCV_Image
+
+def PIL_Scale(Img, Crop_Box, Box_Scale = 1):
+    # Cut down a PIL Image using the box scale provided, using x,y deltas.
+    
+    # Scale x,y
+    x_Delta = max(Crop_Box[2] * (Box_Scale - 1), 0)
+    y_Delta = max(Crop_Box[3] * (Box_Scale - 1), 0)
+
+    # Using x,y convert to PIL Box from CV
+    PIL_Box = [Crop_Box[0] - x_Delta, Crop_Box[1] - y_Delta, Crop_Box[0] + Crop_Box[2] + x_Delta, Crop_Box[1] + Crop_Box[3] + y_Delta]
+
+    return Img.crop(PIL_Box)
+
+def Crop_Face(Img_Pattern, Box_Scale = 1):
+    faceCascade = cv.Load('../cv/haarcascade_frontalface_alt.xml') # Used frontalface_alt instead of _default
+
+    Images = glob.glob(Img_Pattern)
+    if len(Images) <= 0:
+        print 'No Images Found'
+        return
+
+    for Img in Images:
+        PIL_Image = Image.open(Img)
+        OpenCV_Image = Grey_Scale(PIL_Image)
+        Faces = Detect_Face(OpenCV_Image,faceCascade)
+        if Faces:
+            n=1
+            for Face in Faces:
+                Cropped_Image = PIL_Scale(PIL_Image, Face[0], Box_Scale)
+                fname, ext = os.path.splitext(Img)
+                Cropped_Image.save(fname+'_crop'+str(n)+ext)
+                n+=1
+
+        else:
+            print 'No faces found: ', Img
+
+# Crop all jpegs in a folder. Note: the code uses glob which follows unix shell rules.
+# Use the boxScale to scale the cropping area. 1=opencv box, 2=2x the width and height
+Crop_Face('../img/face.jpg', Box_Scale = 1)
