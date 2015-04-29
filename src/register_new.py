73571e142480740611b6c0db0459928fc567c864
sIurps <s94ahmad@gmail.com>
2015-04-29 17:21:01 +0100

    New register file fixes

--- src/register_new.py
+++ src/register_new.py
@@ -1,10 +1,9 @@
-import sys
-import mmap
 import glob, os
 import cv2 as cv
 from direc import alphFolder
-from Crop import Start_Crop
+from Crop import Start_Crop, clean
 from database import *
+import re
 
 class Register(object):
     def __init__(self, firstname, lastname, passphrase, group, room):
@@ -15,56 +14,72 @@ class Register(object):
         self.room = room
         self.name = str(lastname)+"_"+str(firstname) # lastname_forename format for folder names
         self.name = self.name.lower()
-        self.userpath = "../usr/"+self.passphrase[:1]+"/"+self.name
+        self.userpath = ""
         alphFolder()
 
-    def submit(self, groups, userpath, rooms, imgnum):
+    def submit(self, imgnum):
         os.system('rsync %s/face%d_crop.jpg c1312433@lapis.cs.cf.ac.uk:/home/c1312433/CM2301/%s/%s' % (self.userpath, imgnum, self.userpath[3:8], self.userpath[9:]))
-        db = database.database()
-        db.add_user(self, self.userpath, self.passphrase, self.firstname, self.lastname, groups, rooms)
+        
+        ## not working
+        #db = database()
+        #db.add_user(self.userpath, self.passphrase, self.firstname, self.lastname, self.group, self.room)
 
 
-    def CreateFolder(self, name, passphrase):
-        path = "../usr/"+passphrase[:1]+"/"+name
+    def CreateFolder(self):
+        path = "../usr/"+self.passphrase[:1]+"/"+self.name
         print path
 
         if not os.path.exists(path):
             os.makedirs(path)
             with open(path+"/speech.txt", "w+") as theirfile: # Then save a personal record of their passphrase in their folder
-                    theirfile.write(str(passphrase))
+                    theirfile.write(str(self.passphrase))
 
 
         print "Initiating remote sync"
         os.system('rsync --recursive %s c1312433@lapis.cs.cf.ac.uk:/home/c1312433/CM2301/%s/' % (self.userpath, self.userpath[3:8]))
         print "Done."
 
-    def folderCheck(self, name, passphrase):
+        print "User folder created!\nStarting face registration - Please keep still!"
+        
+        for x in range(4,-1,-1):
+            time.sleep(1)
+            print "Capturing in.." + str(x+1)        
 
-        foldercheck = 0
+        print "Capturing now - Please keep still, whilst facing the camera!"
+
+    def folderCheck(self):
+        
+        f = open('../passphrases.txt', 'a+')
+        print "Pulling relevant files from remote server"
+        os.system("rsync -chavzP --stats c1312433@lapis.cs.cf.ac.uk:/home/c1312433/CM2301/usr/%s ../usr/" % (self.passphrase[:1]))
+        print "Done."
+
+        foldercheck = 1
         while(True):
-            if os.path.exists("../usr/"+passphrase[:1]+"/"+name):   
+            if os.path.exists("../usr/"+self.passphrase[:1]+"/"+ self.name):   
                 notfound = 1
                 if notfound:
-                    if name[-1].isdigit() == True:
-                        slicenum = len(str(int(re.search(r'\d+', name).group())))
-                        name = name[:-slicenum]
-                        name+=str(foldercheck)
+                    if self.name[-1].isdigit() == True:
+                        slicenum = len(str(int(re.search(r'\d+', self.name).group())))
+                        self.name = self.name[:-slicenum]
+                        self.name+=str(foldercheck)
 
                     else:
-                        name+=str(foldercheck)
+                        self.name+=str(foldercheck)
 
-                    print "The name " + name[:-1] + " was taken, re-assigned: " + name
+                    print "The name " + self.name[:-1] + " was taken, reassigned: " + self.name
                     foldercheck+=1
 
 
             else:
-                print "Found name: " + str(name)
-                f.write(str(passphrase)+"\n") # MOVE TO ELSE NOW
+                print "Found name: " + str(self.name)
+                self.userpath = "../usr/"+self.passphrase[:1]+"/"+self.name
+                f.write(str(self.passphrase)+"\n") # MOVE TO ELSE NOW
                 print "Creating user folder.."
-                CreateFolder(name, passphrase)
+                register.CreateFolder()
                 break   
         
-    def capImg(self, userpath):
+    def capImg(self):
         cap = cv.VideoCapture(0)
         cap.set(1, 20.0)
         cap.set(3,640)  
@@ -104,7 +119,7 @@ class Register(object):
 
             print msg
 
-            path = '../usr/'+str(userpath)+"/"
+            path = '../usr/'+str(self.userpath)+"/"
             fname = 'face'
             ext = '.jpg'
             
@@ -140,13 +155,13 @@ class Register(object):
         print "All captured, you may rest now."
         print "Syncing local directory to remote - This may take a while"
         for x in range(0,6):
-            Start_Crop(userpath, x)
-            submit(self.groups, self.userpath, self.rooms, x)
-        os.system('rsync %s/speech.txt c1312433@lapis.cs.cf.ac.uk:/home/c1312433/CM2301/%s/%s' % (userpath, userpath[3:8], userpath[9:]))
+            Start_Crop(self.userpath, x)
+            register.submit(x)
+        os.system('rsync %s/speech.txt c1312433@lapis.cs.cf.ac.uk:/home/c1312433/CM2301/%s/%s' % (self.userpath, self.userpath[3:8], self.userpath[9:]))
         print "Sync complete.\n"
 
-        clean(userpath)
-        print "User's path is: " + userpath
+        clean(self.userpath)
+        print "User's path is: " + self.userpath
         print "FINISHED\n"
         #Start_Crop(userpath)
 
@@ -155,7 +170,7 @@ class Register(object):
 if __name__ == '__main__':
     try:
         register = Register("firstname", "lastname", "passphrase", "group", "room")
-        register.folderCheck("name", "passphrase")
-        register.capImg("userpath")
+        register.folderCheck()
+        register.capImg()
     except (Exception) as err:
         print err
