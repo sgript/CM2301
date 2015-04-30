77befe653fed1c5a640067e7ff9586083d2e50ad
sIurps <s94ahmad@gmail.com>
2015-04-30 19:17:59 +0100

    comments for direc.py

--- src/direc.py
+++ src/direc.py
@@ -2,12 +2,12 @@
 import os
 
 def alphFolder():
-	path = "../usr/"
-	letter = "a"
-	for x in range(0,26):
-		if not os.path.exists(path+letter):
-			os.makedirs(path+letter)
-			letter = chr(ord(letter) + 1)
+	path = "../usr/" # path to create on
+	letter = "a" # starts from the letter "a"
+	for x in range(0,26): 
+		if not os.path.exists(path+letter): 
+			os.makedirs(path+letter) # make directory for each letter
+			letter = chr(ord(letter) + 1) 
 			
 		
 	print "Folder structure in place.\nContinuing.."
